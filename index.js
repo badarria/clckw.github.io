833953ac0a60d3bd6e7352f41792b77863534d03
badarria <baklykovadaria@gmail.com>
2020-11-14 18:34:15 +0200

    added app.get to client folder

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ app.use('/', routes)
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "client/build")));
-	// app.get('*', function(req, res) {
-	// 	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 }
 
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+})
 
 app.listen(PORT, () => {
 	console.log(`Server has started on port ${PORT}..`)

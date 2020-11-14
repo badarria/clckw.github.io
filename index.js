7afea26dfbb29f7ba13612b3527ae5a8ce905b99
badarria <baklykovadaria@gmail.com>
2020-11-14 18:16:27 +0200

    added whitelist

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://test-clckw.herokuapp.com']
+const corsOptions = {
+	origin: function (origin, callback) {
+		console.log("** Origin of request " + origin)
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			console.log("Origin acceptable")
+			callback(null, true)
+		} else {
+			console.log("Origin rejected")
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
+}
+app.use(cors(corsOptions))
 
 app.use(cors())
 app.use(express.json());
@@ -14,7 +28,9 @@ app.use('/', routes)
 
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")))
+	app.use(express.static(path.join(__dirname, "client/build")));
+	// app.get('*', function(req, res) {
+	// 	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 }
 
 

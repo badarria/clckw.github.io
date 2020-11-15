5f0a9b653b133a91c94b31a68020e41665088349
badarria <baklykovadaria@gmail.com>
2020-11-15 15:42:45 +0200

    after successful deployment with auth bug fixed

--- index.js
+++ index.js
@@ -6,21 +6,6 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
-//
-// const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://test-clckw.herokuapp.com']
-// const corsOptions = {
-// 	origin: function (origin, callback) {
-// 		console.log("** Origin of request " + origin)
-// 		if (whitelist.indexOf(origin) !== -1 || !origin) {
-// 			console.log("Origin acceptable")
-// 			callback(null, true)
-// 		} else {
-// 			console.log("Origin rejected")
-// 			callback(new Error('Not allowed by CORS'))
-// 		}
-// 	}
-// }
-// app.use(cors(corsOptions))
 
 app.use(cors())
 app.use(express.json());

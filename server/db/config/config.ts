7b3a37e56dfd4fceaaf7f367421662fd1af0b9c0
badarria <baklykovadaria@gmail.com>
2021-03-18 11:32:26 +0200

    fixed sequelize config

--- server/db/config/config.ts
+++ server/db/config/config.ts
@@ -9,10 +9,12 @@ export default {
     dialect: 'postgres',
   },
   production: {
-    username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    host: process.env.PG_HOST,
-    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 }

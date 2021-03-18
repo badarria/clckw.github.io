ecec70b671920e2220d78db67535b5c912ba9bf6
badarria <baklykovadaria@gmail.com>
2021-03-18 11:44:46 +0200

    added ssl to seq.options

--- server/db/index.ts
+++ server/db/index.ts
@@ -3,7 +3,11 @@ import { config } from '../../config'
 import { City, Customer, Service, Master, Admin, Order } from './models'
 const connectionString = config.db
 
-const sequelize = new Sequelize(connectionString)
+const sequelize = new Sequelize(connectionString, {
+  dialectOptions: {
+    ssl: true,
+  },
+})
 sequelize.addModels([City, Customer, Service, Master, Admin, Order])
 
 export { sequelize }

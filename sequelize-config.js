1d2d5b0c470452891990047272f621f0ce64d854
badarria <baklykovadaria@gmail.com>
2021-04-13 18:25:40 +0300

    fixed sequelize-cli config

--- sequelize-config.js
+++ sequelize-config.js
@@ -18,6 +18,7 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    ssl: true,
     dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
   }
 }
